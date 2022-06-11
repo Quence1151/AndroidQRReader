4bcf85c137e11e8f618dbf9af345828323d967a4
Quence1151 <dlgksdyd1151@naver.com>
2022-06-11 14:19:19 +0900

    project init

---
+++ app/src/main/java/kr/co/androidqrreader/AnyOrientationCaptureActivity.java
@@ -0,0 +1,6 @@
+package kr.co.androidqrreader;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+}
---
+++ app/src/main/java/kr/co/androidqrreader/MainActivity.java
@@ -0,0 +1,74 @@
+package kr.co.androidqrreader;
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTest;
+    static String string;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new IntentIntegrator(MainActivity.this).initiateScan();
+
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        //  com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
+        //  = 0x0000c0de; // Only use bottom 16 bits
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if (result == null) {
+                // 취소됨
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            } else {
+                // 스캔된 QRCode --> result.getContents()
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                NetworkTask2 networkTask = new NetworkTask2();
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("value", result.getContents());
+                networkTask.execute(params);
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public class NetworkTask2 extends AsyncTask<Map<String, String>, Integer, String> {
+        @Override
+        protected String doInBackground(Map<String, String>... maps) { // 내가 전송하고 싶은 파라미터
+            // Http 요청 준비 작업
+            HttpClient.Builder http = new HttpClient.Builder("POST", "http://kosa2.iptime.org:50232/postTest");
+            // Parameter 를 전송한다.
+            http.addAllParameters(maps[0]);
+            //Http 요청 전송
+            HttpClient post = http.create();
+            post.request();
+            // 응답 상태코드 가져오기
+            int statusCode = post.getHttpStatusCode();
+            // 응답 본문 가져오기
+            String body = post.getBody();
+            return body;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
+    }
+}
